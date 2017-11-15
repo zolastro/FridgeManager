67aac5cc275775000a70b2cc6c5d8d0ac8726694
David Ponce <zolastrodavid@gmail.com>
2017-11-15 21:36:21 +0100

    Added "confirm purchase" alert

--- src/pages/fridge/fridge.ts
+++ src/pages/fridge/fridge.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import { ProductsService } from '../../services/products.service';
-import { DatabaseService } from '../../services/database.service';
 import {User} from 'firebase/app';
 import {Product} from '../../models/product';
 import {BalanceService} from '../../services/balance.service';
-import {ToastController} from 'ionic-angular';
+import {AlertController, ToastController} from 'ionic-angular';
 import { ProductPage } from '../product/product';
 
 @Component({
@@ -15,7 +14,6 @@ import { ProductPage } from '../product/product';
 export class FridgePage implements OnInit{
   products = [];
   user: User;
-  money: Number;
   productPage = ProductPage;
 
   constructor(
@@ -23,7 +21,7 @@ export class FridgePage implements OnInit{
     private productsService: ProductsService,
     private balanceService: BalanceService,
     private toastCtrl: ToastController,
-    private databaseService: DatabaseService
+    private alertCtrl: AlertController
   ) { }
 
   ngOnInit() {
@@ -45,15 +43,39 @@ export class FridgePage implements OnInit{
     this.authService.login();
   }
 
-  buyProduct(product: Product) {
+  onBuyProduct(product: Product) {
     if (this.user) {
-      this.balanceService.buyProduct(product, this.user);
+      this.confirmPurchase(product);
     } else {
       let toast = this.toastCtrl.create({
         message: 'You must be logged in!',
-        duration: 3000
+        duration: 2000
       });
       toast.present();
     }
+  }
+
+  confirmPurchase(product: Product) {
+    let confirm = this.alertCtrl.create({
+      title: 'Do you want to buy' + product.name +'?',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role:'cancel',
+          handler: () => {
+            console.log('Purchase Cancelled');
+          }
+        },
+        {
+          text: 'Go ahead',
+          handler: () => {
+            this.balanceService.buyProduct(product, this.user);
+            console.log('Purchase Adquired');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 }
